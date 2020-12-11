99ae7cc7c4fc1a2e18dc77df164d0b4941ab7dd3
Georgiana Druță <75542257+georgianadruta@users.noreply.github.com>
2020-12-11 09:58:01 +0200

    Update proj.v

--- proj.v
+++ proj.v
@@ -29,5 +29,55 @@ Definition update (env : Env) (s : string) (x : nat) : Env :=
   fun y => if (eqb y s)
               then x
               else (env y).
-              
- Check env.
+
+Check env.
+
+Reserved Notation "A =[ S ]=> N" (at level 70).
+
+Inductive aeval : AExp -> Env -> nat -> Prop :=
+| const : forall n sigma, anum n =[ sigma ]=> n 
+| var : forall v sigma, astring v =[ sigma ]=> (sigma v) 
+| add' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = i1 + i2 ->
+    a1 +' a2 =[sigma]=> n
+| sub' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    i1 > i2 ->
+    n = i1 - i2 ->
+    a1 -' a2 =[sigma]=> n
+| times' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = i1 * i2 ->
+    a1 *' a2 =[sigma]=> n
+| div' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    i2 >0 -> 
+    n = Nat.div i1 i2 ->
+    a1 /' a2 =[sigma]=> n
+| mod' : forall a1 a2 i1 i2 sigma n,
+    a1 =[ sigma ]=> i1 ->
+    a2 =[ sigma ]=> i2 ->
+    n = Nat.modulo i1 i2 ->
+    a1 %' a2 =[sigma]=> n
+
+where "a =[ sigma ]=> n" := (aeval a sigma n).
+
+Hint Constructors aeval.
+
+(* Fixpoint aeval (a : AExp) (env : Env) : nat :=
+  match a with
+  | anum n' => n'
+  | astring s' => s'
+  | aplus a1 a2 => (aeval a1 env) + (aeval a2 env)
+  | aminus a1 a2 => (aeval a1 env) + (aeval a2 env)
+  | amul a1 a2 => (aeval a1 env) * (aeval a2 env)
+  | adiv a1 a2 => Nat.div (aeval a1 env) (aeval a2 env)
+  | amod a1 a2 => Nat.modulo (aeval a1 env) (aeval a2 env)
+  end. *)
+  
+  
